ca5e6135491c2d570ad2421fc0ae98ee35780033
Sammy Tam <sammytam2005@hotmail.com>
2016-09-14 23:40:39 +0000

    modified

--- downloadAvatars.js
+++ downloadAvatars.js
@@ -10,19 +10,17 @@ const repo = process.argv[3];
 function downloadAvatars(){
   if (process.argv.length !== 4) {
     console.log("Incorrect number of arguments given to program \nPlease enter arguments in this order: \n1. Repository Owner \n2. Repository Name");
-  }
-
-  else {
+  } else {
     fs.stat('./.env', function (err, stats) {
       if (err) {
         return console.log('Missing .env file with configuration information');
       }
 
-    const userName = process.env['USER_NAME'];
-    const api_token = process.env['GITHUB_API_TOKEN'];
+      const userName = process.env['USER_NAME'];
+      const api_token = process.env['GITHUB_API_TOKEN'];
 
-    const endpoint = 'http://' + userName + ':' + api_token + '@api.github.com';
-    getAvatar.get(owner, repo, endpoint);
+      const endpoint = 'http://' + userName + ':' + api_token + '@api.github.com';
+      getAvatar.get(owner, repo, endpoint);
     });
   }
 }
